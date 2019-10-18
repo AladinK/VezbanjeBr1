b924fd01ee575939e5aade737e1731abe6ef7faf
AladinK <56448422+AladinK@users.noreply.github.com>
2019-10-18 16:34:10 +0200

    Add project files.

---
+++ VezbanjeBroj2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VezbanjeBroj2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite prvi broj");
+            string unos = Console.ReadLine();
+            int broj1 = int.Parse(unos);
+
+            Console.WriteLine("Unesite drugi broj");
+            unos = Console.ReadLine();
+            int broj2 = int.Parse(unos);
+
+            int zbir = broj1 + broj2;
+
+            if (zbir > 10 && zbir < 20) //Prvi uslov
+            {
+                Console.WriteLine("Zbir je veci od 10 i manji od 20!!");
+            }
+            else if (zbir < 5) //Drugi uslov
+            {
+                Console.WriteLine("Manji od 5!");
+            }
+            else //Ako nista nije ispunjeno onda ovo
+            {
+                Console.WriteLine("Izmedju 5 i 10!");
+            }
+            int x = 5;
+            int y = 5;
+            if (x == y)
+            {
+                Console.WriteLine("Prva grana");
+
+            } else if (x == y)
+            {
+                Console.WriteLine("Druga grana");
+
+            } 
+            Console.WriteLine($"ZBIR JE {broj1 + broj2}.");
+            Console.ReadKey();
+
+               
+
+        }
+    }
+}
